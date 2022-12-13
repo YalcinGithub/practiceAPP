d3360f8ccc1dd3f5bfa8e7581deadc6388ea51b7
Burhan Kuvvetlioglu <bkuvvetlioglu@hotmail.com>
2022-12-13 21:42:55 +0300

    Created Us35 and committed

---
+++ src/com/cydeo/Us35_PrintIssue.java
@@ -0,0 +1,8 @@
+package com.cydeo;
+
+public class Us35_PrintIssue {
+    public static void main(String[] args) {
+        System.out.println("This is a print test");
+        System.out.println("I love this project this project");
+    }
+}
